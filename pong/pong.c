b97b8ca41fe05192d126384a5f2a102617b787ed
dpv927 <dpv927@inlumine.ual.es>
2024-02-21 23:46:46 +0100

    update

--- pong/pong.c
+++ pong/pong.c
@@ -1,4 +1,28 @@
-#include "raylib.h"
+#include <raylib.h>
+
+#define WINDOW_WIDTH     800
+#define WINDOW_HEIGHT    450
+#define PAD_WIDTH        25
+#define PAD_HEIGHT       100
+#define DEF_PAD_X        50
+#define DEF_PAD_Y        175
+#define DEF_BALL_X       WINDOW_WIDTH>>1
+#define DEF_BALL_Y       WINDOW_HEIGHT>>1
+#define DEF_BALL_SPEED_X 5
+#define DEF_BALL_SPEED_Y 5
+#define BALL_RADIUS      10
+
+enum { 
+  NO_PLAYER = 0,
+  PLAYER_1,
+  PLAYER_2,
+};
+
+typedef struct{
+  int x;
+  int y;
+  int radius;
+} Circle;
 
 void movePaddle(int keyPressed, Rectangle* paddle, int windowHeight, int speedY) {
   int newY = paddle->y + speedY;
@@ -9,30 +33,71 @@ void movePaddle(int keyPressed, Rectangle* paddle, int windowHeight, int speedY)
   } 
 }
 
+int moveBall(Vector2* ball, Vector2* ballSpeed) {
+  int newX = ball->x + ballSpeed->x;
+  int newY = ball->y + ballSpeed->y;
+
+  if(newX <= BALL_RADIUS)
+    return PLAYER_2;
+  if(newX >= WINDOW_WIDTH - BALL_RADIUS)
+    return PLAYER_1;
+  
+  if(newY <= BALL_RADIUS || newY >= WINDOW_HEIGHT - BALL_RADIUS)
+    ballSpeed->y *= -1;
+
+  ball->x += ballSpeed->x;
+  ball->y += ballSpeed->y; 
+  return NO_PLAYER;
+}
+
+void manageColisions(Vector2 ball, Vector2* ballSpeed, Rectangle recPad1, Rectangle recPad2) {
+  Vector2 bCenter = {ball.x, ball.y};
+
+  if(CheckCollisionCircleRec(bCenter, BALL_RADIUS, recPad1)){
+    if((bCenter.y - BALL_RADIUS <= recPad1.y) || 
+        (bCenter.y + BALL_RADIUS >= recPad1.y + recPad1.height))
+      ballSpeed->y *= -1;
+    else { ballSpeed->x *= -1; }
+  }
+
+  if(CheckCollisionCircleRec(bCenter, BALL_RADIUS, recPad2)) {
+    if((bCenter.y - BALL_RADIUS <= recPad2.y) || 
+        (bCenter.y + BALL_RADIUS >= recPad2.y + recPad2.height))
+      ballSpeed->y *= -1;
+    else { ballSpeed->x *= -1; }
+  }
+}
+
 int main(void) {
-  
-  Vector2 window = { 800, 450 };
+
   Vector2 paddleSpeed = { 0, 12 };  
-  Vector2 ballSpeed = { 5, 5 };
-  int ballRadius = 10;
+  int gameState;
 
-  Vector2 ballCircle;
-  ballCircle.x = (int)window.x>>1;
-  ballCircle.y = (int)window.y>>1;
+  Vector2 ballSpeed = {
+    .x = DEF_BALL_SPEED_X,
+    .y = DEF_BALL_SPEED_Y 
+  };
+  
+  Vector2 ball = {
+    .x = DEF_BALL_X,
+    .y = DEF_BALL_Y,
+  };
 
-  Rectangle recPad1;
-  recPad1.x = 50;
-  recPad1.y = 175;
-  recPad1.width = 25,
-  recPad1.height = 100;
-  
-  Rectangle recPad2;
-  recPad2.x = window.x - (recPad1.x+recPad1.width);
-  recPad2.y = recPad1.y;
-  recPad2.width = recPad1.width;
-  recPad2.height = recPad1.height;
-  
-  InitWindow(window.x, window.y, "basic window");
+  Rectangle recPad1 = {
+    .x = DEF_PAD_X,
+    .y = DEF_PAD_Y,
+    .width = PAD_WIDTH,
+    .height = PAD_HEIGHT,
+  };
+
+  Rectangle recPad2 = {
+    .x = WINDOW_WIDTH - (DEF_PAD_X + PAD_WIDTH),
+    .y = DEF_PAD_Y,
+    .width = PAD_WIDTH,
+    .height = PAD_HEIGHT,
+  };
+
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "basic window");
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
@@ -41,17 +106,27 @@ int main(void) {
     DrawFPS(10, 10);
 
     // Check Pad1 movement
-    movePaddle(KEY_DOWN, &recPad1, window.y, paddleSpeed.y);
-    movePaddle(KEY_UP,   &recPad1, window.y, paddleSpeed.y*(-1));
+    movePaddle(KEY_S, &recPad1, WINDOW_HEIGHT, paddleSpeed.y);
+    movePaddle(KEY_W,   &recPad1, WINDOW_HEIGHT, paddleSpeed.y*(-1));
     
     // Check Pad2 movement
-    movePaddle(KEY_S, &recPad2, window.y, paddleSpeed.y);
-    movePaddle(KEY_W, &recPad2, window.y, paddleSpeed.y*(-1));
-  
+    movePaddle(KEY_DOWN, &recPad2, WINDOW_HEIGHT, paddleSpeed.y);
+    movePaddle(KEY_UP, &recPad2, WINDOW_HEIGHT, paddleSpeed.y*(-1));
+
+    // Move the ball
+    manageColisions(ball, &ballSpeed, recPad1, recPad2);
+    gameState = moveBall(&ball, &ballSpeed);
+    
+    if(gameState) { // One of the players won
+      char* message = (gameState == PLAYER_1)? 
+        "Player 1 won!" : "Player 2 won!";
+      DrawText(message, 285, 200, 40, LIGHTGRAY);
+    }
+
     // Draw all the elements
     DrawRectangleRec(recPad1, BLUE);
     DrawRectangleRec(recPad2, RED);
-    DrawCircleV(ballCircle, ballRadius, RAYWHITE);
+    DrawCircle(ball.x, ball.y, BALL_RADIUS, RAYWHITE);
     EndDrawing();
   }
 
