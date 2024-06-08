49dd83652522c729e0167adf09fce1e6ee96c460
dpv927 <dpv927@inlumine.ual.es>
2024-06-08 19:52:41 +0200

    update

--- Pong/pong.c
+++ Pong/pong.c
@@ -1,132 +1,164 @@
+#include <string.h>
 #include <raylib.h>
+#include <stdbool.h>
 
 #define WINDOW_WIDTH     800
-#define WINDOW_HEIGHT    450
+#define WINDOW_HEIGHT    500
 #define PAD_WIDTH        25
 #define PAD_HEIGHT       100
 #define DEF_PAD_X        50
-#define DEF_PAD_Y        175
-#define DEF_BALL_X       WINDOW_WIDTH>>1
-#define DEF_BALL_Y       WINDOW_HEIGHT>>1
+#define PADDLE_SPEED     10
 #define DEF_BALL_SPEED_X 5
 #define DEF_BALL_SPEED_Y 5
-#define BALL_RADIUS      10
+#define BALL_WIDTH       20
+#define FONTSIZE         40
 
-enum { 
-  NO_PLAYER = 0,
-  PLAYER_1,
-  PLAYER_2,
+const int DEF_PAD_Y  = (WINDOW_HEIGHT>>1) - (PAD_HEIGHT>>1);
+const int DEF_BALL_X = WINDOW_WIDTH>>1;
+const int DEF_BALL_Y = WINDOW_HEIGHT>>1;
+
+struct Player {
+  Rectangle paddle;
+  int key_up;
+  int key_down;
+  int speed;
 };
 
-typedef struct{
-  int x;
-  int y;
-  int radius;
-} Circle;
+struct Ball {
+  Rectangle shape;
+  Vector2 speed;
+};
 
-void movePaddle(int keyPressed, Rectangle* paddle, int windowHeight, int speedY) {
-  int newY = paddle->y + speedY;
+enum GameState {
+  NO_WIN = 0,
+  PLAYER1_WON,
+  PLAYER2_WON,
+};
 
-  if(IsKeyDown(keyPressed) && (newY+paddle->height < windowHeight) 
-    && (newY > 0)) {
-    paddle->y += speedY;
-  } 
-}
-
-int moveBall(Vector2* ball, Vector2* ballSpeed) {
-  int newX = ball->x + ballSpeed->x;
-  int newY = ball->y + ballSpeed->y;
-
-  if(newX <= BALL_RADIUS)
-    return PLAYER_2;
-  if(newX >= WINDOW_WIDTH - BALL_RADIUS)
-    return PLAYER_1;
+void checkPaddleMovement(struct Player* player) {
+  int moveDir; // Moving direction
   
-  if(newY <= BALL_RADIUS || newY >= WINDOW_HEIGHT - BALL_RADIUS)
-    ballSpeed->y *= -1;
-
-  ball->x += ballSpeed->x;
-  ball->y += ballSpeed->y; 
-  return NO_PLAYER;
+  moveDir = IsKeyDown(player->key_down) - IsKeyDown(player->key_up);
+  if(player->paddle.y == 0 && moveDir == -1) return;
+  if(player->paddle.y + player->paddle.height == WINDOW_HEIGHT && moveDir == 1) return;
+  player->paddle.y += moveDir * player->speed;
 }
 
-void manageColisions(Vector2 ball, Vector2* ballSpeed, Rectangle recPad1, Rectangle recPad2) {
-  Vector2 bCenter = {ball.x, ball.y};
+bool checkPaddleCollision(struct Ball* ball, struct Player player) {
+  
+  if (CheckCollisionRecs(ball->shape, player.paddle)) {
+    float paddleCenterY;
+    float ballCenterY;
+    float collisionPoint;
 
-  if(CheckCollisionCircleRec(bCenter, BALL_RADIUS, recPad1)){
-    if((bCenter.y - BALL_RADIUS <= recPad1.y) || 
-        (bCenter.y + BALL_RADIUS >= recPad1.y + recPad1.height))
-      ballSpeed->y *= -1;
-    else { ballSpeed->x *= -1; }
-  }
+    // Determine collision point relative to paddle
+    paddleCenterY = player.paddle.y + player.paddle.height/2.0f;
+    ballCenterY = ball->shape.y + ball->shape.height/2.0f;
+    collisionPoint = ballCenterY - paddleCenterY;
 
-  if(CheckCollisionCircleRec(bCenter, BALL_RADIUS, recPad2)) {
-    if((bCenter.y - BALL_RADIUS <= recPad2.y) || 
-        (bCenter.y + BALL_RADIUS >= recPad2.y + recPad2.height))
-      ballSpeed->y *= -1;
-    else { ballSpeed->x *= -1; }
+    // Normalize collision point (-1 to 1 range)
+    collisionPoint /= (player.paddle.height/2);
+
+    // Reflect ball
+    ball->speed.x *= -1;
+    ball->speed.y += collisionPoint * 2.0f;
+    return true;
   }
+  return false;
+}
+
+int moveBall(struct Ball* ball) {
+  int newX = ball->shape.x + ball->speed.x;
+  int newY = ball->shape.y + ball->speed.y;
+
+  if (newX <= 0)
+    return PLAYER2_WON;
+  if (newX >= WINDOW_WIDTH - BALL_WIDTH)
+    return PLAYER1_WON;
+    
+  if (newY <= 0 || newY >= WINDOW_HEIGHT - BALL_WIDTH)
+    ball->speed.y *= -1;
+
+  ball->shape.x = newX;
+  ball->shape.y = newY; 
+  return NO_WIN;
 }
 
 int main(void) {
 
-  Vector2 paddleSpeed = { 0, 12 };  
-  int gameState;
+  bool gameAlive = true;
+  int winner;
 
-  Vector2 ballSpeed = {
-    .x = DEF_BALL_SPEED_X,
-    .y = DEF_BALL_SPEED_Y 
-  };
-  
-  Vector2 ball = {
-    .x = DEF_BALL_X,
-    .y = DEF_BALL_Y,
-  };
-
-  Rectangle recPad1 = {
-    .x = DEF_PAD_X,
-    .y = DEF_PAD_Y,
-    .width = PAD_WIDTH,
-    .height = PAD_HEIGHT,
+  struct Player player1 = {
+    .paddle = {
+      .x = DEF_PAD_X,
+      .y = DEF_PAD_Y,
+      .width = PAD_WIDTH,
+      .height = PAD_HEIGHT,
+    },
+    .key_down = KEY_S,
+    .key_up = KEY_W,
+    .speed = PADDLE_SPEED
   };
 
-  Rectangle recPad2 = {
-    .x = WINDOW_WIDTH - (DEF_PAD_X + PAD_WIDTH),
-    .y = DEF_PAD_Y,
-    .width = PAD_WIDTH,
-    .height = PAD_HEIGHT,
+  struct Player player2 = {
+    .paddle = {
+      .x = WINDOW_WIDTH - (DEF_PAD_X + PAD_WIDTH),
+      .y = DEF_PAD_Y,
+      .width = PAD_WIDTH,
+      .height = PAD_HEIGHT,
+    },
+    .key_down = KEY_DOWN,
+    .key_up = KEY_UP,
+    .speed = PADDLE_SPEED
   };
 
-  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "basic window");
+  struct Ball ball = {
+    .shape = {
+      .x = DEF_BALL_X,
+      .y = DEF_BALL_Y,
+      .width = BALL_WIDTH,
+      .height = BALL_WIDTH
+    },
+    .speed = {
+      DEF_BALL_SPEED_X,
+      DEF_BALL_SPEED_Y
+    }
+  };
+
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawFPS(10, 10);
 
-    // Check Pad1 movement
-    movePaddle(KEY_S, &recPad1, WINDOW_HEIGHT, paddleSpeed.y);
-    movePaddle(KEY_W,   &recPad1, WINDOW_HEIGHT, paddleSpeed.y*(-1));
-    
-    // Check Pad2 movement
-    movePaddle(KEY_DOWN, &recPad2, WINDOW_HEIGHT, paddleSpeed.y);
-    movePaddle(KEY_UP, &recPad2, WINDOW_HEIGHT, paddleSpeed.y*(-1));
+    if(gameAlive /* Game keeps going */) {
+      checkPaddleMovement(&player1);
+      checkPaddleMovement(&player2);
 
-    // Move the ball
-    manageColisions(ball, &ballSpeed, recPad1, recPad2);
-    gameState = moveBall(&ball, &ballSpeed);
-    
-    if(gameState) { // One of the players won
-      char* message = (gameState == PLAYER_1)? 
-        "Player 1 won!" : "Player 2 won!";
-      DrawText(message, 285, 200, 40, LIGHTGRAY);
+      if((winner = moveBall(&ball))) {
+        // Someone won
+        gameAlive = false;
+      }
+
+      // Check colisiones
+      if (checkPaddleCollision(&ball,player1) 
+        || checkPaddleCollision(&ball, player2)) {
+        ball.shape.x += ball.speed.x;
+      }
+
+      // Draw all the elements
+      DrawRectangleRec(player1.paddle, BLUE);
+      DrawRectangleRec(player2.paddle, RED);
+      DrawRectangleRec(ball.shape, RAYWHITE);
     }
-
-    // Draw all the elements
-    DrawRectangleRec(recPad1, BLUE);
-    DrawRectangleRec(recPad2, RED);
-    DrawCircle(ball.x, ball.y, BALL_RADIUS, RAYWHITE);
+    else {
+      // Show game end information1
+      int positiony = (WINDOW_HEIGHT>>1) - FONTSIZE;
+      int positionx = (WINDOW_WIDTH>>1) - (FONTSIZE*(int)strlen("Player1 won"))/3;
+      DrawText(TextFormat("Player%d won!", winner), positionx, positiony, FONTSIZE, WHITE);
+    }
     EndDrawing();
   }
 
