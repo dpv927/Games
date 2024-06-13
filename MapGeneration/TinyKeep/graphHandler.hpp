00abbbe3e62bc1fc088260b91aeb153965973cc4
dpv927 <dpv927@inlumine.ual.es>
2024-06-13 20:56:37 +0200

    update

--- MapGeneration/TinyKeep/graphHandler.hpp
+++ MapGeneration/TinyKeep/graphHandler.hpp
@@ -1,16 +1,28 @@
 #ifndef GRAPH_HANDLER
 
-#include <vector>
+#include<vector>
 #include "room.hpp"
 
 namespace graphHandler {
 
-  struct Graph {
-    std::vector<std::vector<std::pair<int,int>>> adjs;
-    
-    Graph(const long vertices);   
-    void addEdge(const int u, const int v, const long w);
-  };
+  struct Graph { 
+	  int V;
+    int E;
+    std::vector<std::pair<int, std::pair<int, int>>> edges;
+
+	  Graph(std::vector<TinyKeep::Room>& rooms);
+	  void addEdge(const int u, const int v, const int w);
+    void computeKruskalMST(void); 
+  }; 
+
+  struct DisjointSet { 
+	  int *parent, *rnk; 
+	  int n; 
+
+  	DisjointSet(int n); 
+    int find(int u); 
+	  void merge(int x, int y);
+  }; 
 
   Graph* getRoomsTriangulationGraph(std::vector<TinyKeep::Room>& rooms);
   void getMinimumSpanningTree(Graph* graph);
