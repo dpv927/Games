f9af5ae349b98f6f4654edf45c4cdeab20c97bc1
dpv927 <dpv927@inlumine.ual.es>
2024-10-28 21:37:46 +0100

    update

--- Spawn/entities/player.py
+++ Spawn/entities/player.py
@@ -14,20 +14,22 @@ class Player(TexturedEntity):
         self.lerp_y = self.position.y
         self.buffs = []
 
-
     def move(self):
         move_x = int(rl.is_key_down(rl.KeyboardKey.KEY_D)) - int(rl.is_key_down(rl.KeyboardKey.KEY_A))
         move_y = int(rl.is_key_down(rl.KeyboardKey.KEY_S)) - int(rl.is_key_down(rl.KeyboardKey.KEY_W))
-
-        if move_x != 0 and move_y != 0:
-            self.lerp_x += (move_x)/2. * self.speed.x
-            self.lerp_y += (move_y)/2. * self.speed.y
-        else:
-            self.lerp_x += move_x * self.speed.x
-            self.lerp_y += move_y * self.speed.y
-    
+        movement = rl.vector2_normalize(rl.Vector2(move_x, move_y))
+        
+        self.lerp_x += movement.x * self.speed.x
+        self.lerp_y += movement.y * self.speed.y
+        
         self.position.x = rl.lerp(self.position.x, self.lerp_x, 0.15)
         self.position.y = rl.lerp(self.position.y, self.lerp_y, 0.15)
+
+    def set_position(self, x, y):
+        self.position.x = x 
+        self.position.y = y 
+        self.lerp_x = x
+        self.lerp_y = y
 
 # Public player instance
 player_instance = Player()
--- Spawn/main.py
+++ Spawn/main.py
@@ -1,28 +1,63 @@
+from entities.player import player_instance as player
 import pyray as rl 
 import random
 import registry
-from entities.player import player_instance
+
+def scale_window():
+    monitor = rl.get_current_monitor()
+    mon_height = rl.get_monitor_height(monitor)
+    mon_width = rl.get_monitor_width(monitor)
+    
+    width = int(mon_width * 0.5)
+    height = int(mon_height * 0.5)
+    
+    rl.set_window_position(int((mon_width - width)/2), int((mon_height - height)/2))
+    rl.set_window_size(width, height)
+    return width, height
+
 
 if __name__ == '__main__':
-    window_width = 1000 
-    window_height = 1000
 
-    rl.init_window(window_width, window_height, "Spawn")
-    rl.set_target_fps(60)
-
+    #cl = random.choice(list(registry.entity_register.values()))
+    #entity = cl()
+    
+    # Load resources before creating the window
     registry.load_entities_modules() 
+
+    rl.init_window(500, 500, "Spawn")
+    rl.set_target_fps(60)
+    w, h = scale_window()
     registry.load_textures()
 
-    cl = random.choice(list(registry.entity_register.values()))
-    entity = cl()
-    player = player_instance
-    
+    # Player 2D camera
+    camera = rl.Camera2D(rl.Vector2(w/2, h/2), rl.Vector2(0,0), 0, 1)
+
+    # Create a list with enemies arround the player
+    # TODO Create a function that spawns entities arround a 
+    # circle (out of the window)
+    enemies = []
+    for i in range(10):
+        entity_class = random.choice(list(registry.entity_register.values()))
+        entity = entity_class()
+        enemies.append(entity)
+
     while(not rl.window_should_close()):
         player.move()
+        camera.target.x = player.position.x
+        camera.target.y = player.position.y
+        
+        for enemy in enemies:
+            enemy.move()
+    
         rl.begin_drawing()
-        rl.clear_background(rl.WHITE)
-        entity.draw()
+        rl.clear_background(rl.BROWN)
+        rl.begin_mode_2d(camera)
         player.draw()
+    
+        for enemy in enemies:
+            enemy.draw()
+
+        rl.end_mode_2d()
         rl.end_drawing()
 
     registry.unload_textures()
